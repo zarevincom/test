ad9c7260bde3c3f782f89c4d381d9a141ea78509
DESKTOP-EIP30D5\zarevincom <zavikgamesone@gmail.com>
2021-01-15 01:43:03 +0300

    Added new routes & controllers (auth, analytics, position, order, category)

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+//routePaths
 const authRoutes = require('./routes/auth')
-const mainRoute = require('./routes/main')
+const analyticsRoutes = require('./routes/analytics')
+const positionRoutes = require('./routes/position')
+const orderRoutes = require('./routes/order')
+const categoryRoutes = require('./routes/category')
 const app = express()
+//App
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-
-app.use('/api/auth', authRoutes)
-app.use('/', mainRoute)
+app.use('/api/client/auth', authRoutes)
+app.use('/api/client/analytics', analyticsRoutes)
+app.use('/api/client/position', positionRoutes)
+app.use('/api/client/order', orderRoutes)
+app.use('/api/client/category', categoryRoutes)
 
 
 module.exports = app
