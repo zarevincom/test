82944b76070d57c0aeda71f60e966cb91aa20eed
DESKTOP-EIP30D5\zarevincom <zavikgamesone@gmail.com>
2021-01-15 01:43:22 +0300

    no message

--- controllers/main.js
+++
@@ -1,8 +0,0 @@
-const { json } = require("express")
-module.exports.getName =  function(req, res) {
-    res.status(200).json({
-        login: req.body,
-        pass: req.body
-    })
-    console.log(res.body)
-}
--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/main')
-
-
-router.get('/', controller.getName)
-
-module.exports = router
